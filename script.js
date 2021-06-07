68a8de9117fae007b566381b7a82456dad8781b2
Marcello <marcellogodenzi@hotmail.it>
2021-06-07 19:24:03 -0300

    Modify function playRound

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ playerSelection = prompt("Choose between Rock, Paper or Scissors");
 computerSelection = computerPlay();  
 if (playerSelection.toLowerCase() == "rock" && computerSelection == "rock"){
 return ("Rock vs Rock, nobody wins");}
-
+else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper"){
+    return ("You lose, paper beats rock!");}
+else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors"){
+    return ("You win, rock beats scissors!");}
+else if(playerSelection.toLowerCase() == "paper" && computerSelection == "paper"){
+    return ("Paper vs Paper, nobody wins");}
+else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
+    return ("You win, paper beats rock!");}
+else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors"){
+    return ("You lose, scissors beats paper!");}
+else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock"){
+    return ("You lose, rock beats scissors");}
+else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper"){
+    return ("You win, scissors beats paper!");}
+else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "scissors"){
+    return ("Scissors vs Scissors, nobody wins");}
 }
 playRound(playerSelection,computerSelection)
+
+console.log(playRound(playerSelection, computerSelection));
